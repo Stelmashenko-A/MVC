e8377800f047103e0d80521415fdc8f0b324f8a5
Stelmashenko-A <stelma_28@bk.ru>
2015-08-26 22:37:00 +0300

    Add Account Repository

---
+++ MVC/MVC/Repository/AccountRepository.cs
@@ -0,0 +1,42 @@
+﻿using System.Data.Entity.Core;
+using System.Linq;
+using MVC.AOP;
+using MVC.Models;
+
+namespace MVC.Repository
+{
+    internal class AccountRepository : Repository<User>, IAccountRepository
+    {
+        [TransactionAspect]
+        public User GetUser(string name, string passHash)
+        {
+            try
+            {
+                return NewDataBaseContext.Users
+                    .FirstOrDefault(u => u.Name == name && u.Password == passHash);
+            }
+
+            catch (EntityException)
+            {
+                return null;
+            }
+        }
+
+        [TransactionAspect]
+        public AccountRepositoryState AddUnique(User user)
+        {
+            var tmp = NewDataBaseContext.Users
+                .FirstOrDefault(u => u.Name == user.Name && u.Password == user.Password);
+            if (tmp != null) return AccountRepositoryState.UserIsNotUnique;
+
+            user.Id = user.GetHashCode();
+            NewDataBaseContext.Users.Add(user);
+            NewDataBaseContext.SaveChanges();
+
+            user = NewDataBaseContext.Users
+                .FirstOrDefault(u => u.Name == user.Name && u.Password == user.Password);
+
+            return user != null ? AccountRepositoryState.Success : AccountRepositoryState.SavingError;
+        }
+    }
+}
